65df896fc0b44f0b4fe5d4ffb953b1a4a1a2b358
igorjovita <igor.jovita@hotmail.com>
2023-11-01 10:20:53 -0300

    Initial commit

---
+++ database.py
@@ -0,0 +1,33 @@
+import mysql.connector
+import os
+import dotenv
+
+mydb = mysql.connector.connect(
+    host=os.getenv("DB_HOST"),
+    user=os.getenv("DB_USERNAME"),
+    passwd=os.getenv("DB_PASSWORD"),
+    db=os.getenv("DB_NAME"),
+    autocommit=True,
+    ssl_verify_identity=False,
+    ssl_ca=r"C:\users\acqua\downloads\cacert-2023-08-22.pem")
+
+cursor = mydb.cursor()
+
+
+def lançamento_caixa(data_caixa, id_staff, tipo_lancamento, pagamento_entrada, descricao, valor_entrada):
+    cursor.execute("INSERT INTO lancamento_caixa(data, id_staff, tipo_lancamento,  forma_pg  , descricao, valor) VALUES(%s, %s, %s, %s, %s, %s)", (data_caixa, id_staff, tipo_lancamento, pagamento_entrada, descricao, valor_entrada))
+    mydb.commit()
+
+
+def cliente(cpf, nome_cliente, telefone_cliente, email_cliente, peso, altura, cidade, estado):
+    cursor.execute("INSERT INTO cliente (cpf, nome, telefone, email, peso, altura, cidade, estado) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)", (cpf, nome_cliente, telefone_cliente, email_cliente, peso, altura, cidade, estado))
+
+
+def cadastrar_vendedor(nome_vendedor, telefone_vendedor, apelido_vendedor, valor_neto):
+    cursor.execute("INSERT INTO vendedores(nome, telefone, apelido, valor neto) VALUES (%s, %s, %s, %s)", (nome_vendedor, telefone_vendedor, apelido_vendedor, valor_neto))
+
+
+def lista_vendedores():
+    cursor.execute("SELECT apelido FROM vendedores")
+    dados = cursor.fetchall()
+    return dados
---
+++ main.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import pandas as pd
+from streamlit_option_menu import option_menu
+from database import lista_vendedores
+
+escolha = option_menu(menu_title="Planilha Diaria", options=['Reservar', 'Visualizar', 'Editar', 'Pagamento'], icons=['book', 'card-checklist', 'pencil-square', 'currency-dollar'],
+                          orientation='horizontal')
+
+if escolha == 'Reservar':
+    lista = lista_vendedores()
+    st.subheader('Reservar Clientes')
+
+    data = st.date_input('Data da Reserva', format='DD/MM/YYYY')
+    nome = st.text_input('Nome do Cliente :').replace(' ', '_')
+    telefone = st.text_input('Telefone do Cliente :')
+    comissario = st.selectbox('Vendedor :', lista, placeholder=' ')
+    tipo = st.selectbox('Modalidade : ', ('', 'BAT', 'TUR1', 'TUR2', 'OWD', 'ADV'), placeholder='Vendedor')
+    altura = st.slider('Altura do Cliente', 1.50, 2.10)
+    peso = st.slider('Peso do Cliente', 40, 160)
+    sinal = st.text_input('Valor do Sinal')
+    recebedor_sinal = st.selectbox('Quem recebeu o sinal?', ['', 'AcquaWorld', 'Vendedor'])
+    valor_loja = st.number_input('Receber na Loja :', format='%d', step=10)
+
