3a5fb1e8402e053c7e1cba0dec37b273308afcea
igorjovita <igor.jovita@hotmail.com>
2023-11-01 13:36:29 -0300

    c0.2

--- database.py
+++ database.py
@@ -12,15 +12,15 @@ mydb = mysql.connector.connect(
     ssl_ca=r"C:\users\acqua\downloads\cacert-2023-08-22.pem")
 
 cursor = mydb.cursor()
-
+chars = "'),([]"
 
 def lançamento_caixa(data_caixa, id_staff, tipo_lancamento, pagamento_entrada, descricao, valor_entrada):
     cursor.execute("INSERT INTO lancamento_caixa(data, id_staff, tipo_lancamento,  forma_pg  , descricao, valor) VALUES(%s, %s, %s, %s, %s, %s)", (data_caixa, id_staff, tipo_lancamento, pagamento_entrada, descricao, valor_entrada))
     mydb.commit()
 
 
-def cliente(cpf, nome_cliente, telefone_cliente, email_cliente, peso, altura, cidade, estado):
-    cursor.execute("INSERT INTO cliente (cpf, nome, telefone, email, peso, altura, cidade, estado) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)", (cpf, nome_cliente, telefone_cliente, email_cliente, peso, altura, cidade, estado))
+def cliente(cpf, nome_cliente, telefone_cliente, peso, altura):
+    cursor.execute("INSERT INTO cliente (cpf, nome, telefone, peso, altura) VALUES (%s, %s, %s, %s, %s, )", (cpf, nome_cliente, telefone_cliente, peso, altura))
 
 
 def cadastrar_vendedor(nome_vendedor, telefone_vendedor, apelido_vendedor, valor_neto):
@@ -35,3 +35,15 @@ def lista_vendedores():
 
 def vendas(data, id_cliente, id_vendedor, pago_loja, pago_vendedor):
     cursor.execute("INSERT INTO vendas (data, id_cliente, id_vendedor, pago_loja, pago_vendedor) VALUES (%s, %s, %s, %s, %s)", (data, id_cliente, id_vendedor, pago_loja, pago_vendedor))
+
+
+def id_vendedor(vendedor):
+    cursor.execute(f"SELECT id FROM vendedores WHERE nome = {vendedor}")
+    id_ven = str(cursor.fetchone()).translate(str.maketrans('', '', chars))
+    return id_ven
+
+
+def id_cliente(nome):
+    cursor.execute(f"SELECT id FROM cliente WHERE nome = {nome}")
+    id_ven = str(cursor.fetchone()).translate(str.maketrans('', '', chars))
+    return id_cli
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 from streamlit_option_menu import option_menu
-from database import lista_vendedores
+from database import lista_vendedores, cliente, vendas, id_vendedor, id_cliente
 
 escolha = option_menu(menu_title="Planilha Diaria", options=['Reservar', 'Visualizar', 'Editar', 'Pagamento'], icons=['book', 'card-checklist', 'pencil-square', 'currency-dollar'],
                           orientation='horizontal')
@@ -19,7 +19,22 @@ if escolha == 'Reservar':
     tipo = st.selectbox('Modalidade : ', ('', 'BAT', 'TUR1', 'TUR2', 'OWD', 'ADV'), placeholder='Vendedor')
     altura = st.slider('Altura do Cliente', 1.50, 2.10)
     peso = st.slider('Peso do Cliente', 40, 160)
+    valor_mergulho = st.text_input('Valor do Mergulho')
     sinal = st.text_input('Valor do Sinal')
     recebedor_sinal = st.selectbox('Quem recebeu o sinal?', ['', 'AcquaWorld', 'Vendedor'])
+    if recebedor_sinal == 'AcquaWorld':
+        pago_loja = sinal
+        pago_vendedor = 0
+
+    if recebedor_sinal == 'Vendedor':
+        pago_loja = 0
+        pago_vendedor = sinal
+
     valor_loja = st.number_input('Receber na Loja :', format='%d', step=10)
 
+    if st.button('Reservar'):
+        cliente(cpf, nome, telefone, peso, altura)
+        id_vendedor(comissario)
+        id_cliente(nome)
+        vendas(data, id_cliente, id_vendedor, pago_loja, pago_vendedor)
+        st.success('Reserva realizada com sucesso!')
