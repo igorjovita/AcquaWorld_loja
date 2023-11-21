b4d59fae4361b6fe69219f560369465207b21ed8
igorjovita <igor.jovita@hotmail.com>
2023-11-20 22:48:55 -0300

    Update main.py

--- main.py
+++ main.py
@@ -149,6 +149,7 @@ if escolha == 'Editar':
     mydb.connect()
     cursor.execute(f"select cliente.nome from reserva join cliente on cliente.id = reserva.id_cliente  where data = '{nova_data}'")
     lista_clientes = cursor.fetchall()
+    st.write(lista_clientes)
     for i, cliente in enumerate(lista_clientes):
         nome_cli = str(cliente[i]).translate(str.maketrans('', '', chars2))
     st.write(nome_cli)
