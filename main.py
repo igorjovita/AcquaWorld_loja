018fcc3baa5d80b1612852bd78b4847aab905f58
igorjovita <igor.jovita@hotmail.com>
2023-11-20 23:34:26 -0300

    Update main.py

--- main.py
+++ main.py
@@ -148,7 +148,7 @@ if escolha == 'Editar':
         cursor.execute(f"select r.data, c.nome, c.cpf, c.telefone, v.nome , r.tipo, r.fotos, c.altura, c.peso from reserva as r join cliente as c on c.id = r.id_cliente join vendedores as v on v.id = r.id_vendedor where data = '{nova_data}' and c.nome = '{novo_nome}'")
         reserva_selecionada = cursor.fetchall()
         reserva = str(reserva_selecionada).translate(str.maketrans('', '', chars2)).split(',')
-        data = (reserva[0])[12:]
+        data = (reserva[0])[13:]
         st.subheader(data)
 
 
