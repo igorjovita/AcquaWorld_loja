bd65316b035dbca4716fa8be69b91949620f087b
igorjovita <igor.jovita@hotmail.com>
2023-11-27 14:24:40 -0300

    Update main.py

--- main.py
+++ main.py
@@ -266,6 +266,9 @@ if escolha == 'Pagamento':
             sinal_pg = 0
             recebedor_sinal_pg = None
 
+        st.write(info_reserva_pg[2])
+        st.write(info_reserva_pg[3])
+
         cursor.execute("INSERT INTO pagamentos (data, id_reserva, id_vendedor, sinal, recebedor_sinal, pagamento, forma_pg, parcela) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)", (data_pagamento, info_reserva_pg[0], info_reserva_pg[1], sinal_pg, recebedor_sinal_pg, pagamento, forma_pg, parcela))
         mydb.close()
         st.success('Pagamento lançado no sistema!')
