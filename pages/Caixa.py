546dfa487b8409262b1bc3a64d689b79f1b1a4ea
igorjovita <igor.jovita@hotmail.com>
2023-11-27 16:00:12 -0300

    Update Caixa.py

--- pages/Caixa.py
+++ pages/Caixa.py
@@ -64,12 +64,11 @@ if escolha == 'Caixa Diario':
 
     dividido = str(controle).split(',')
     st.write(dividido)
-    if dividido[1] is not None:
+    if dividido is not None:
 
         entradas = (str(dividido[1]).replace('Decimal', '').translate(str.maketrans('', '', chars)))
         entrada_final = str(entradas).replace('.', ',')
         st.subheader(f'    - Total de Entradas : R$ {entrada_final}')
-    if dividido[3] is not None:
         saidas = (str(dividido[3]).replace('Decimal', '').translate(str.maketrans('', '', chars)))
         saida_final = str(saidas).replace('.', ',')
         st.subheader(f'    - Total de Saidas : R$ {saida_final}')
