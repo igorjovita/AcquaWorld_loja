fa460a21d10e0b630d41bdbc1b0a0324ee39b881
igorjovita <igor.jovita@hotmail.com>
2023-12-01 16:55:25 -0300

    c

--- pages/Comissão.py
+++ pages/Comissão.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 from streamlit_option_menu import option_menu
-from database import lista_vendedores, cliente, vendas, id_vendedor, id_cliente
+
 import os
 import mysql.connector
 from datetime import date, datetime
