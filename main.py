b7d9ad4da34661161d7dc98bd6bc7d334128a57b
igorjovita <igor.jovita@hotmail.com>
2023-12-05 06:30:57 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import base64
+
 import streamlit as st
 import pandas as pd
 from streamlit_option_menu import option_menu
@@ -229,11 +231,18 @@ if escolha == 'Visualizar':
     st.dataframe(df, hide_index=True, width=100)
 
     pdf = FPDF('P', 'mm', 'Letter')
+
+
+    def create_download_link(val, filename):
+        b64 = base64.b64encode(val)  # val looks like b'...'
+        return f'<a href="data:application/octet-stream;base64,{b64.decode()}" download="{filename}.pdf">Download file</a>'
     if st.button('Criar Pdf'):
         pdf.add_page()
         pdf.set_font('helvetica', '', 16)
         pdf.cell(40, 10, 'Hello World')
-        pdf.output('pdf_1.pdf')
+        html = create_download_link(pdf.output(dest="S").encode("latin-1"), "test")
+
+        st.markdown(html, unsafe_allow_html=True)
 
 if escolha == 'Pagamento':
     data_pagamento = date.today()
