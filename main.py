a60c2d7d3c063043718d8e9539882a8562e9ce8a
IgorJovita <igor.jovita@hotmail.com>
2023-12-21 16:25:39 -0300

    Update main.py

--- main.py
+++ main.py
@@ -195,7 +195,8 @@ if escolha == 'Visualizar':
     df = pd.DataFrame(planilha, columns=['Nome', 'Cpf', 'Telefone', 'Comissário', 'Cert', 'Fotos', 'Roupa', 'Check_in'])
 
     # Criar DataFrame estilizado
-    styled_df = df.style.applymap(lambda x: get_color(df['Check_in'].iloc[x]) if x == 'Nome' else '')
+    styled_df = df.style.apply(lambda row: [get_color(row['Check_in'])] * len(row.index),
+                               subset=pd.IndexSlice[:, ['Nome']])
 
     # Exibir DataFrame estilizado
     st.dataframe(styled_df, hide_index=True)
