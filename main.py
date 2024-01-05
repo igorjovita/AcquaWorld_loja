17f67c7b8aede119a453a7e4f6a76be6f2d1798c
IgorJovita <igor.jovita@hotmail.com>
2024-01-05 00:40:48 -0300

    Update main.py

--- main.py
+++ main.py
@@ -245,7 +245,7 @@ if escolha == 'Reservar':
             with mydb.cursor() as cursor:
                 cursor.execute(f"SELECT id_cliente, nome_cliente FROM reserva where id_titular = id_cliente and data = '{data}'")
                 id_reserva_conjunta = cursor.fetchall()[0]
-                nome_reserva_conjunta = str(cursor.fetchoall())[1].translate(str.maketrans('', '', chars2)).split(',')
+                nome_reserva_conjunta = str(cursor.fetchall())[1].translate(str.maketrans('', '', chars2)).split(',')
                 st.write(id_reserva_conjunta)
                 st.write(nome_reserva_conjunta)
                 for nome in nome_reserva_conjunta:
