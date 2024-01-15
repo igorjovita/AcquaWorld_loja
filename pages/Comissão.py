2fc6c31b831efc6c53b78f5bd0abcb8699bcfd53
IgorJovita <igor.jovita@hotmail.com>
2024-01-15 06:33:08 -0300

    Update Comissão.py

--- pages/Comissão.py
+++ pages/Comissão.py
@@ -119,7 +119,7 @@ if st.button('Pesquisar Comissão'):
     df_final = pd.concat([df, df_soma])
     # Remover a última linha (soma total) antes de exibir a tabela
 
-    col2.dataframe(df.drop(columns=['Selecionar']).style.format({
+    col2.dataframe(df.style.format({
         'Valor Receber': 'R${:,.2f}',
         'Valor Pagar': 'R${:,.2f}'
     }).set_properties(**{'text-align': 'center'}).set_table_styles([{
