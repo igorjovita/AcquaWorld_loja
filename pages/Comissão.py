91ecca0bb20f30cf6ec5b929b5af98bc1d247f9b
IgorJovita <igor.jovita@hotmail.com>
2024-01-19 15:48:50 -0300

    Update Comissão.py

--- pages/Comissão.py
+++ pages/Comissão.py
@@ -142,7 +142,7 @@ if st.button('Pesquisar Comissão', on_click=pressionar) or st.session_state.bot
             total_receber = st.session_state.df_state.loc[
                 st.session_state.df_state['Selecionar'], 'Valor a Receber'].sum()
 
-            total = str(total_pagar).replace('R$', '')
+            total = str(total_pagar).replace('R$', '').split()
 
             st.write(total)
 
