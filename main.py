2aa5b9b2dde0b6ed79adb4397be36c7411021a38
IgorJovita <igor.jovita@hotmail.com>
2024-01-25 15:04:32 -0300

    Update main.py

--- main.py
+++ main.py
@@ -246,9 +246,14 @@ if escolha == 'Reservar':
     ids_clientes = []
     nomes_clientes = []
     nomes_titulares = []
-    valor_sinal = 0
-    valor_mergulho_total = 0
-    valor_mergulho_receber = 0
+    if 'valor_sinal' not in st.session_state:
+        st.session_state.valor_sinal = 0
+
+    if 'valor_mergulho_total' not in st.session_state:
+        st.session_state.valor_mergulho_total = 0
+
+    if 'valor_mergulho_receber' not in st.session_state:
+        st.session_state.valor_mergulho_receber = 0
     with col1:
         data = st.date_input('Data da Reserva', format='DD/MM/YYYY')
         comissario = st.selectbox('Vendedor:', lista_vendedor, index=None, placeholder='Escolha o vendedor')
@@ -336,9 +341,9 @@ if escolha == 'Reservar':
                     roupa = f'{altura}/{peso}'
 
                 if st.button(f'Cadastrar {nome_cliente}', key=f'button{i}'):
-                    valor_sinal += float(sinal)
-                    valor_mergulho_receber += float(valor_loja)
-                    valor_mergulho_total += float(valor_mergulho)
+                    st.session_state.valor_sinal += float(sinal)
+                    st.session_state.valor_mergulho_receber += float(valor_loja)
+                    st.session_state.valor_mergulho_total += float(valor_mergulho)
                     with mydb.cursor() as cursor:
                         try:
                             cursor.execute(
@@ -453,9 +458,9 @@ if escolha == 'Reservar':
                             Reserva Concluida com Sucesso!
                             
                                 Titular da Reserva - {nome_titular}
-                                Valor total - {valor_mergulho_total}
-                                Já foi pago - {valor_sinal}
-                                Pagar no dia {data} - {valor_mergulho_receber}
+                                Valor total - {st.session_state.valor_mergulho_total}
+                                Já foi pago - {st.session_state.valor_sinal}
+                                Pagar no dia {data} - {st.session_state.valor_mergulho_receber}
                                 
                             """)
 
