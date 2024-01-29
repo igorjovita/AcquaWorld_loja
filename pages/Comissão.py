aea00f9fa91a6c6cfe27f12879acf6da00acc821
IgorJovita <igor.jovita@hotmail.com>
2024-01-29 10:48:05 -0300

    Update Comissão.py

--- pages/Comissão.py
+++ pages/Comissão.py
@@ -184,9 +184,7 @@ if st.button('Pesquisar Comissão', on_click=pressionar) or st.session_state.bot
                         id_titular = cursor.fetchone()[0]
                         st.write(id_titular)
                         cursor.execute(f"SELECT id from lancamento_comissao where id_titular = {id_titular}")
-                        result = cursor.fetchone()
-                        if result:
-                            id_comissao = int(result[0])
+                        id_comissao = cursor.fetchone()[0]
                         lista_ids.append(id_comissao)
                     for numero in lista_ids:
                         if recebedor == f'{comissario} receber':
