f9b641428900925b074b134558a86b2d7bf9460c
IgorJovita <igor.jovita@hotmail.com>
2024-01-31 18:07:18 -0300

    Update main.py

--- main.py
+++ main.py
@@ -918,77 +918,77 @@ if escolha == 'Pagamento':
                 if check_in_entry == 'Para o pier':
                     check_in = 'yellow'
 
-                if st.button('Lançar Pagamento'):
-                    if pagamento_escolha == 'Pagamento Grupo':
-                        for nome in lista_nome_pagamento:
-                            processar_pagamento(nome, cursor, data_reserva, check_in, forma_pg, parcela, id_vendedor_pg, id_titular_pagamento)
-                    else:
-                        nome = escolha_client_input
-                        processar_pagamento(nome, cursor, data_reserva, check_in, forma_pg, parcela, id_vendedor_pg,
-                                            id_titular_pagamento)
-
-                        # info_reserva = obter_info_reserva(cursor, nome, data_reserva)
-                        #
-                        # update_check_in(cursor, nome, check_in)
-                        #
-                        # id_reserva_cliente = info_reserva[0]
-                        # id_cliente_pg = info_reserva[1]
-                        # tipo = info_reserva[2]
-                        # valor_total_reserva = info_reserva[3]
-                        # receber_loja_individual = info_reserva[4]
-                        #
-                        # pagamento = receber_loja_individual
-                        # recebedor_pagamento = 'AcquaWorld'
-                        #
-                        # id_pagamento = insert_pagamento(cursor, data_pagamento, id_reserva_cliente, recebedor, pagamento, forma_pg, parcela, id_titular_pagamento)
-                        #
-                        # cursor.execute(
-                        #     f"SELECT recebedor, sum(pagamento) from pagamentos where id_reserva = {id_reserva_cliente} group by recebedor")
-                        # resultado_soma = cursor.fetchall()
-                        # st.write(resultado_soma)
-                        #
-                        # vendedor_nome = None
-                        # vendedor_valor = None
-                        # acquaworld_nome = None
-                        # acquaworld_valor = None
-                        #
-                        # valor_neto = obter_valor_neto(cursor, tipo, valor_total_reserva, id_vendedor_pg)
-                        # reserva_neto = valor_total_reserva - valor_neto
-                        #
-                        # for result in resultado_soma:
-                        #     nome_result = result[0]
-                        #     valor = result[1]
-                        #
-                        #     if nome_result == 'Vendedor':
-                        #         vendedor_nome = nome_result
-                        #         vendedor_valor = valor
-                        #
-                        #     elif nome_result == 'AcquaWorld':
-                        #         acquaworld_nome = nome_result
-                        #         acquaworld_valor = valor
-                        #
-                        # valor_receber, valor_pagar, situacao = calcular_valores(valor_neto, acquaworld_valor, vendedor_valor, reserva_neto)
-                        #
-                        # data_completa = str(data_reserva).split('-')
-                        # descricao = f'{nome} do dia {data_completa[2]}/{data_completa[1]}/{data_completa[0]}'
-                        # tipo_movimento = 'Entrada'
-                        # id_conta = 1
-                        #
-                        # insert_caixa(cursor, id_conta, data_pagamento, tipo_movimento, tipo, descricao, forma_pg, pagamento)
-                        #
-                        # insert_lancamento_comissao(cursor, id_reserva_cliente, id_vendedor_pg, valor_receber, valor_pagar, situacao, id_titular_pagamento)
-
-                    st.session_state.pagamentos = []
-                    st.session_state.pagamentos2 = []
-
-                    mydb.close()
-                    st.success('Pagamento lançado no sistema!')
-                    st.session_state.botao = False
-
+            if st.button('Lançar Pagamento'):
+                if pagamento_escolha == 'Pagamento Grupo':
+                    for nome in lista_nome_pagamento:
+                        processar_pagamento(nome, cursor, data_reserva, check_in, forma_pg, parcela, id_vendedor_pg, id_titular_pagamento)
                 else:
-                    st.success('Todos os clientes desse grupo já realizaram o pagamento!')
+                    nome = escolha_client_input
+                    processar_pagamento(nome, cursor, data_reserva, check_in, forma_pg, parcela, id_vendedor_pg,
+                                        id_titular_pagamento)
+
+                    # info_reserva = obter_info_reserva(cursor, nome, data_reserva)
+                    #
+                    # update_check_in(cursor, nome, check_in)
+                    #
+                    # id_reserva_cliente = info_reserva[0]
+                    # id_cliente_pg = info_reserva[1]
+                    # tipo = info_reserva[2]
+                    # valor_total_reserva = info_reserva[3]
+                    # receber_loja_individual = info_reserva[4]
+                    #
+                    # pagamento = receber_loja_individual
+                    # recebedor_pagamento = 'AcquaWorld'
+                    #
+                    # id_pagamento = insert_pagamento(cursor, data_pagamento, id_reserva_cliente, recebedor, pagamento, forma_pg, parcela, id_titular_pagamento)
+                    #
+                    # cursor.execute(
+                    #     f"SELECT recebedor, sum(pagamento) from pagamentos where id_reserva = {id_reserva_cliente} group by recebedor")
+                    # resultado_soma = cursor.fetchall()
+                    # st.write(resultado_soma)
+                    #
+                    # vendedor_nome = None
+                    # vendedor_valor = None
+                    # acquaworld_nome = None
+                    # acquaworld_valor = None
+                    #
+                    # valor_neto = obter_valor_neto(cursor, tipo, valor_total_reserva, id_vendedor_pg)
+                    # reserva_neto = valor_total_reserva - valor_neto
+                    #
+                    # for result in resultado_soma:
+                    #     nome_result = result[0]
+                    #     valor = result[1]
+                    #
+                    #     if nome_result == 'Vendedor':
+                    #         vendedor_nome = nome_result
+                    #         vendedor_valor = valor
+                    #
+                    #     elif nome_result == 'AcquaWorld':
+                    #         acquaworld_nome = nome_result
+                    #         acquaworld_valor = valor
+                    #
+                    # valor_receber, valor_pagar, situacao = calcular_valores(valor_neto, acquaworld_valor, vendedor_valor, reserva_neto)
+                    #
+                    # data_completa = str(data_reserva).split('-')
+                    # descricao = f'{nome} do dia {data_completa[2]}/{data_completa[1]}/{data_completa[0]}'
+                    # tipo_movimento = 'Entrada'
+                    # id_conta = 1
+                    #
+                    # insert_caixa(cursor, id_conta, data_pagamento, tipo_movimento, tipo, descricao, forma_pg, pagamento)
+                    #
+                    # insert_lancamento_comissao(cursor, id_reserva_cliente, id_vendedor_pg, valor_receber, valor_pagar, situacao, id_titular_pagamento)
+
+                st.session_state.pagamentos = []
+                st.session_state.pagamentos2 = []
+
+                mydb.close()
+                st.success('Pagamento lançado no sistema!')
+                st.session_state.botao = False
+
             else:
-                st.error('Nenhuma reserva para essa data')
+                st.success('Todos os clientes desse grupo já realizaram o pagamento!')
+        else:
+            st.error('Nenhuma reserva para essa data')
 #     st.write('---')
 #
 #     st.subheader('Limitar Vagas')
