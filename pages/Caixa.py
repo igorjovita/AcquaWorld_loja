71f5809848a0f67b76cf2528a3b6354276ed5c3d
IgorJovita <igor.jovita@hotmail.com>
2024-02-06 10:38:19 -0300

    c

--- pages/Caixa.py
+++ pages/Caixa.py
@@ -30,7 +30,7 @@ html_content = None
 
 with col1:
     if st.button('Abrir Total'):
-        st.components.v1.html(gerar_html_caixa(data_caixa), height=1000, width=1000, scrolling=True)
+        st.components.v1.html(gerar_html_caixa(data_caixa), height=900, width=900, scrolling=True)
 
 with col2:
     if st.button('Abrir Entrada'):
