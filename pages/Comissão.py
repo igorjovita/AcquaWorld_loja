3145e220b7c95e528e2758ce96a5d5704a3bda35
IgorJovita <igor.jovita@hotmail.com>
2024-02-08 20:56:05 -0300

    Update Comissão.py

--- pages/Comissão.py
+++ pages/Comissão.py
@@ -195,7 +195,7 @@ if st.button('Pesquisar Comissão', on_click=pressionar) or st.session_state.bot
             st.write(lista_titular)
             st.write(lista_data)
             with mydb.cursor() as cursor:
-                for titular, data in lista_titular, lista_data:
+                for titular, data in zip(lista_titular, lista_data):
                     cursor.execute(f"SELECT id_cliente from reserva where nome_cliente = '{titular} and data = '{data}'")
                     id_titular = cursor.fetchone()[0]
                     st.write(id_titular)
