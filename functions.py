b2a81bf8cfc57c1c06525f182051272ec8da95ff
IgorJovita <igor.jovita@hotmail.com>
2024-02-19 16:17:01 -0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -493,7 +493,7 @@ def gerar_html(data_para_pdf):
         for i in range(numero):
             html_table += f"""
             <tr style="height: 30px;">
-                <td style="text-align: center;">{numero - i}</td>
+                <td style="text-align: center;">{numero + i}</td>
                 <td></td>
                 <td></td>
                 <td></td>
