ef6d389e23003064ceef5bb3176ca5b753826f9d
IgorJovita <igor.jovita@hotmail.com>
2024-02-19 12:38:12 -0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -446,6 +446,8 @@ def gerar_html(data_para_pdf):
     cursor.execute(f"SELECT c.nome AS nome_cliente, c.cpf, c.telefone, v.apelido AS nome_vendedor, r.tipo, r.fotos, r.dm, c.roupa FROM reserva AS r INNER JOIN cliente AS c ON r.id_cliente = c.id INNER JOIN vendedores AS v ON r.id_vendedor = v.id WHERE r.data = '{data_para_pdf}'")
     dados = cursor.fetchall()
 
+
+
     # Inicialize a variável html_table fora do loop
     # Criando a tabela em HTML com bordas e estilo de layout automático
     html_table = """
@@ -488,6 +490,20 @@ def gerar_html(data_para_pdf):
                 <td>{roupa}</td>
             </tr>
         """
+    if len(dados) < 9:
+        html_table += """
+        <tr style="height: 18px;">
+            <td style="text-align: center;"></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        """ * 9
 
     mydb.close()
     # Fechando a tabela
