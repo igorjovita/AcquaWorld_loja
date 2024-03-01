e6a4352a7ddaac81cf8a875d662f58b4281b2977
IgorJovita <igor.jovita@hotmail.com>
2024-03-01 10:39:18 -0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -232,6 +232,8 @@ def update_vaga(nome_vaga, nome, cpf, telefone, peso, altura, valor_total, sinal
 
     cursor.execute(f"SELECT id FROM cliente where nome LIKE'{nome_vaga}%'")
     id_cliente = cursor.fetchall()
+    for id_ in id_cliente:
+        st.write(id_)
     st.write(id_cliente)
     roupa = f'{altura}/{peso}'
     for reserva in id_cliente:
