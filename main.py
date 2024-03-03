e79ce5e7db95ad9a15fa6dd8814ae0c91dabf6cd
IgorJovita <igor.jovita@hotmail.com>
2024-03-03 00:49:19 -0300

    Update main.py

--- main.py
+++ main.py
@@ -219,7 +219,7 @@ if menu_main == 'Reservar':
                 if st.form_submit_button(f'Cadastrar {nome_cliente}'):
                     if tipo == 'OWD' or tipo == 'ADV':
                         st.date_input('Data da Pratica 2', format='DD/MM/YYYY')
-                        st.button('teste')
+                        st.form_submit_button('teste')
                     if nome_cliente not in st.session_state.nome_cadastrado:
                         st.session_state.nome_cadastrado.append(nome_cliente)
                         forma_pg = 'Pix'
