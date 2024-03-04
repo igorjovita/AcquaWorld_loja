b73ec103a422837fef9f20f60b45a42abfa1a63f
IgorJovita <igor.jovita@hotmail.com>
2024-03-04 14:46:16 -0300

    Update Controles.py

--- pages/Controles.py
+++ pages/Controles.py
@@ -142,7 +142,6 @@ if menu_controles == 'Maquinas':
 
         if st.form_submit_button('Pesquisar'):
             maquina_escolhida = select_maquina_pagamentos(select_box_maquina)
-            st.write(maquina_escolhida)
-            df = pd.DataFrame(maquina_escolhida, ['Data', 'Nome Cliente', 'Curso', 'Forma Pagamento', 'Parcela', 'Valor'])
-            st.write(df)
+            df = pd.DataFrame(maquina_escolhida, columns=['Data', 'Nome Cliente', 'Curso', 'Forma Pagamento', 'Parcela', 'Valor'])
+
             st.dataframe(df, hide_index=True, use_container_width=True)
