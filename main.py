ff312e970415ef8de537c4d272087cb0d0832aef
IgorJovita <igor.jovita@hotmail.com>
2024-03-06 00:50:53 -0300

    Update main.py

--- main.py
+++ main.py
@@ -665,7 +665,7 @@ if menu_main == 'Pagamento':
                     f"<h2 style='color: black; text-align: center; font-size: 1.2em;'>{situacao_pg}</h2>",
                     unsafe_allow_html=True)
 
-            pagamento_individual_coletivo = st.radio('Tipo de pagamento', ['Pagamento Individual', 'Pagamento em Grupo'], horizontal=True)
+        pagamento_individual_coletivo = st.radio('Tipo de pagamento', ['Pagamento Individual', 'Pagamento em Grupo'], horizontal=True)
             
 
 
