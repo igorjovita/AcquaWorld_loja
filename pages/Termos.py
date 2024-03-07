fc348fd2bc1596e6b6e406d5dcda4c215a2eb106
IgorJovita <igor.jovita@hotmail.com>
2024-03-07 01:23:43 -0300

    Update Termos.py

--- pages/Termos.py
+++ pages/Termos.py
@@ -18,31 +18,31 @@ if st.button('Pesquisar'):
 if st.session_state.pesquisa_termo:
     dados = select_termo_cliente(data_termo)
     if dados:
-        st.write(dados)
-        if len(dados) >= 1:
-            st.write(f'Clientes Não Relacionados - {dados[0][1]}')
-            st.write(f'Clientes Relacionados - {dados[1][1]}')
-            nomes_nao_relacionados = str(dados[0][2]).split(',')
+        if len(dados) == 1 and dados[0][0] == 'relacionado ao cliente':
+            st.success('Todos os termos estão relacionados aos clientes')
         else:
-            st.write(dados)
-        select_box_nao_relacionado = st.selectbox('Escolha o cliente para relacionar', nomes_nao_relacionados, index=None)
-        nomes_reservados, nomes_ids_reservados = select_lista_nomes(data_termo)
-        select_box_reservados = st.selectbox('Clientes Reservados', nomes_reservados, index=None)
-        atualizar = st.selectbox('Atualizar informaçoes do cliente?', ['Sim', 'Não'], index=None)
 
-        if st.button('Relacionar'):
+            nomes_nao_relacionados = str(dados[0][2]).split(',')
 
-            id_cliente = ''
-            for cliente in nomes_ids_reservados:
-                if select_box_reservados == cliente[0]:
-                    id_cliente = cliente[1]
-                    tipo = cliente[2]
-            if atualizar == 'Não':
-                st.write(id_cliente)
-                update_termo_cliente(id_cliente, select_box_nao_relacionado)
 
-            else:
-                update_termo_cliente(id_cliente, select_box_nao_relacionado)
-                update_reserva_cliente_termo(data_termo, id_cliente, tipo)
+            select_box_nao_relacionado = st.selectbox('Escolha o cliente para relacionar', nomes_nao_relacionados, index=None)
+            nomes_reservados, nomes_ids_reservados = select_lista_nomes(data_termo)
+            select_box_reservados = st.selectbox('Clientes Reservados', nomes_reservados, index=None)
+            atualizar = st.selectbox('Atualizar informaçoes do cliente?', ['Sim', 'Não'], index=None)
+
+            if st.button('Relacionar'):
+
+                id_cliente = ''
+                for cliente in nomes_ids_reservados:
+                    if select_box_reservados == cliente[0]:
+                        id_cliente = cliente[1]
+                        tipo = cliente[2]
+                if atualizar == 'Não':
+                    st.write(id_cliente)
+                    update_termo_cliente(id_cliente, select_box_nao_relacionado)
+
+                else:
+                    update_termo_cliente(id_cliente, select_box_nao_relacionado)
+                    update_reserva_cliente_termo(data_termo, id_cliente, tipo)
 
 
