55d43f3d89c4056b8264a6dfcd42654d5fbcab18
IgorJovita <igor.jovita@hotmail.com>
2024-03-06 23:56:26 -0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -322,10 +322,11 @@ def select_maquina_pagamentos(maquina):
 def select_termo_cliente(data):
     mydb.connect()
     cursor.execute(f"""
-    SELECT nome CASE WHEN id_cliente IS NOT NULL THEN 'relacionado ao cliente'
+    SELECT  CASE WHEN id_cliente IS NOT NULL THEN 'relacionado ao cliente'
             ELSE 'nao relacionado'
         END AS situacao,
         COUNT(*) AS quantidade
+        GROUP_CONCAT(nome)
     FROM termo_clientes
     WHERE data_reserva = '{data}'
     GROUP BY id_cliente;
