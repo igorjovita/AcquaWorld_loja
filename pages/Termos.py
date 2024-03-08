1eb5b81a0271c820179ec3fb5e5c298f4a4182da
IgorJovita <igor.jovita@hotmail.com>
2024-03-08 13:40:00 -0300

    Update Termos.py

--- pages/Termos.py
+++ pages/Termos.py
@@ -20,7 +20,7 @@ if st.button('Pesquisar'):
 
 if st.session_state.pesquisa_termo:
     dados, lista_relacionado, lista_nao_relacionado, lista_total = select_termo_cliente(data_termo)
-    col1, col2 = st.columns(2)
+
     if dados:
         if len(lista_nao_relacionado) == 0:
             st.success('Todos os termos estão relacionados aos clientes')
@@ -31,21 +31,21 @@ if st.session_state.pesquisa_termo:
             nomes_reservados, nomes_ids_reservados = select_lista_nomes(data_termo)
             select_box_reservados = st.selectbox('Clientes Reservados', nomes_reservados, index=None)
             atualizar = st.selectbox('Atualizar informaçoes do cliente?', ['Sim', 'Não'], index=None)
-            with col1:
-                if st.button('Relacionar'):
+            
+            if st.button('Relacionar'):
 
-                    id_cliente = ''
-                    for cliente in nomes_ids_reservados:
-                        if select_box_reservados == cliente[0]:
-                            id_cliente = cliente[1]
-                            tipo = cliente[2]
-                    if atualizar == 'Não':
-                        st.write(id_cliente)
-                        update_termo_cliente(id_cliente, select_box_nao_relacionado)
+                id_cliente = ''
+                for cliente in nomes_ids_reservados:
+                    if select_box_reservados == cliente[0]:
+                        id_cliente = cliente[1]
+                        tipo = cliente[2]
+                if atualizar == 'Não':
+                    st.write(id_cliente)
+                    update_termo_cliente(id_cliente, select_box_nao_relacionado)
 
-                    else:
-                        update_termo_cliente(id_cliente, select_box_nao_relacionado)
-                        update_reserva_cliente_termo(data_termo, id_cliente, tipo)
+                else:
+                    update_termo_cliente(id_cliente, select_box_nao_relacionado)
+                    update_reserva_cliente_termo(data_termo, id_cliente, tipo)
 
 
 
