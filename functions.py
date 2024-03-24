338fa1d1b96680bdbc64988a781e456d87c9a471
IgorJovita <igor.jovita@hotmail.com>
2024-03-24 08:56:48 -0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -1115,7 +1115,7 @@ def gerar_html_total(data_caixa):
 
     soma_total_entrada = soma_pix + soma_dinheiro + soma_credito + soma_debito
     soma_total_saida = soma_saida_dinheiro + soma_saida_pix + soma_cofre + soma_reembolso
-    saldo_loja = format_currency(soma_dinheiro - (soma_saida_dinheiro + soma_cofre), 'BRL', locale='pt_BR')
+
 
     soma_pix = format_currency(soma_pix, 'BRL', locale='pt_BR')
     soma_dinheiro = format_currency(soma_dinheiro, 'BRL', locale='pt_BR')
@@ -1134,7 +1134,12 @@ def gerar_html_total(data_caixa):
         f"select valor from caixa where tipo_movimento = 'FECHAMENTO' and data < '{data_caixa}' order by data desc limit 1;;")
     mydb.close()
     dado_fechamento = cursor.fetchone()[0]
+
+
     fechamento = format_currency(dado_fechamento, 'BRL', locale='pt_BR')
+
+    saldo_loja = format_currency((soma_dinheiro + float(dado_fechamento) ) - (soma_saida_dinheiro + soma_cofre), 'BRL', locale='pt_BR')
+
     contexto_total = {'soma_pix': soma_pix, 'soma_dinheiro': soma_dinheiro, 'soma_debito': soma_debito,
                       'soma_credito': soma_credito, 'soma_total_entrada': soma_total_entrada,
                       'soma_reembolso': soma_reembolso, 'soma_saida_pix': soma_saida_pix,
