a3b2086b2bafe824d11ef9c0692f2a76bf6ad4f8
IgorJovita <igor.jovita@hotmail.com>
2024-03-24 11:17:45 -0300

    c

--- main.py
+++ main.py
@@ -15,6 +15,23 @@ from functions import select_reserva, processar_pagamento, gerar_pdf, gerar_html
     insert_controle_curso, select_maquina
 import time
 
+import streamlit_authenticator as stauth
+import yaml
+from yaml.loader import SafeLoader
+
+with open('../config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['preauthorized']
+)
+
+authenticator.login()
+
 chars = "'),([]"
 chars2 = "')([]"
 
