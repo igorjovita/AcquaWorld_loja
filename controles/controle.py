e34121bfa1876679c21d4fc8c95afd003a26bcff
IgorJovita <igor.jovita@hotmail.com>
2024-05-24 07:16:27 -0300

    Update controle.py

--- controles/controle.py
+++ controles/controle.py
@@ -180,7 +180,7 @@ class Controle:
                 lista_maquinas.append(item[0])
 
 
-            select_box_maquina = st.selectbox('Escolha a maquina para pesquisar', ['Maquina'], index=None)
+            select_box_maquina = st.selectbox('Escolha a maquina para pesquisar', lista_maquinas, index=None)
 
             if st.form_submit_button('Pesquisar'):
                 maquina_escolhida = self.repository_pagamentos.select_info_pagamento_por_maquina_cartao(
